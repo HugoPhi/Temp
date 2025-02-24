c91a086f5266bc14e1a63eb0404e728071ca9172
HugoPhi <hugonelson07@outlook.com>
2025-02-24 14:02:54 +0800

    add knn expri

--- plugins/excuter.py
+++ plugins/excuter.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 import traceback
 import pandas as pd
+import numpy as np
 from .metric import Metrics
 
 
@@ -14,6 +15,7 @@ class Excuter:
       - 快捷管理训练，测试，日志全过程，并灵活调试Classifier数组里面的各个模型。
       - 开启Log，支持中途运行出错，结果不丢失。
       - 在使用的时候根据需要重写excute(self)方法。
+      - 这个类没有Valid。
 
     Parameters
     ----------
@@ -37,7 +39,7 @@ class Excuter:
 
     def __init__(self, X_train, y_train, X_test, y_test,
                  clf_dict: dict,
-                 metric_list=['acc', 'macro_f1', 'micro_f1', 'avg_recall'],
+                 metric_list=['accuracy', 'macro_f1', 'micro_f1', 'avg_recall'],
                  log=False,
                  log_dir='./log/'):
         '''
@@ -71,7 +73,7 @@ class Excuter:
         self.metric_list = metric_list
         self.log = log
 
-        self.df = pd.DataFrame(columns=['model'] + self.metric_list + ['training time'] + ['testing time'])
+        self.test = pd.DataFrame(columns=['model'] + self.metric_list + ['training time'] + ['testing time'])
 
         # log
         if log:
@@ -95,7 +97,7 @@ class Excuter:
         '''
         保存df到日志
         '''
-        self.df.to_csv(os.path.join(self.log_path, 'result.csv'), index=False)
+        self.test.to_csv(os.path.join(self.log_path, 'test.csv'), index=False)
 
     def excute(self, name, clf):
         '''
@@ -131,7 +133,7 @@ class Excuter:
                 clf.fit(self.X_train, self.y_train)
                 print(f'Train {name} Cost: {clf.get_training_time():.4f} s')
 
-                y_pred = clf.predict_proba(self.X_test)
+                y_pred = clf.predict(self.X_test)
 
                 mtc = Metrics(self.y_test, y_pred)
 
@@ -163,7 +165,7 @@ class Excuter:
             else:
                 raise ValueError(f'{metric} is not in Metric.')
 
-        self.df.loc[len(self.df)] = [name] + [func() for func in func_list] + [clf.get_training_time(), clf.get_testing_time()]
+        self.test.loc[len(self.test)] = [name] + [func() for func in func_list] + [clf.get_training_time(), clf.get_testing_time()]
 
     def run(self, key):
         '''
@@ -225,9 +227,9 @@ class Excuter:
             self.logline(name, mtc, clf)
 
         if sort_by is not None:
-            print(self.df.sort_values(sort_by, ascending=ascending))
+            print(self.test.sort_values(sort_by, ascending=ascending))
         else:
-            print(self.df)
+            print(self.test)
 
     def get_result(self):
         '''
@@ -237,4 +239,153 @@ class Excuter:
         -------
         self.pd : pd.DataFrame
         '''
-        return self.df
+        return self.test
+
+
+class KFlodExcuter(Excuter):
+    def __init__(self, X_train, y_train, X_test, y_test,
+                 clf_dict: dict,
+                 metric_list=['accuracy', 'macro_f1', 'micro_f1', 'avg_recall'],
+                 k=10,
+                 log=False,
+                 log_dir='./log/'):
+        super().__init__(X_train, y_train, X_test, y_test,
+                         clf_dict, metric_list, log, log_dir)
+
+        self.k = k
+        metrics = self.metric_list + ['training time', 'testing time']
+        self.valid = pd.DataFrame(columns=['model'] + [f'{x}_{suffix}' for x in metrics for suffix in ['mean', 'std']])
+
+    def excute(self, name, clf):
+        '''
+        执行实验。
+
+        Notes
+        -----
+          - 这里必须返回一个测试器和一个训练好的分类器，因为写入日志要用。
+
+        Parameters
+        ----------
+        name : str
+            实验的名字。
+        clf : Clfs
+            实验获取的分类器，继承自接口Clfs。
+
+        Returns
+        -------
+        clf : Clfs
+            训练好的分类器。
+        metric: Metrics
+            有记录的Metric实例。
+
+        Examples
+        --------
+
+        可以这么重写：
+        ```python
+        class MyExcuter(Excuter):
+            def excute(self, name, clf):
+                print(f'>> {name}')
+
+                clf.fit(self.X_train, self.y_train)
+                print(f'Train {name} Cost: {clf.get_training_time():.4f} s')
+
+                y_pred = clf.predict(self.X_test)
+
+                mtc = Metrics(self.y_test, y_pred)
+
+                return mtc, clf
+        ```
+        '''
+        print(f'>> {name}')
+
+        # k折交叉验证
+        k_fold_x_train = np.array_split(self.X_train, self.k)
+        k_fold_y_train = np.array_split(self.y_train, self.k)
+        mtcs = []
+        for i in range(self.k):
+            x_train = np.concatenate(k_fold_x_train[:i] + k_fold_x_train[i + 1:])
+            y_train = np.concatenate(k_fold_y_train[:i] + k_fold_y_train[i + 1:])
+            x_test = k_fold_x_train[i]
+            y_test = k_fold_y_train[i]
+            clf.fit(x_train, y_train)
+
+            y_pred = clf.predict(x_test)
+            mtc = Metrics(y_test, y_pred, proba=False)
+            mtcs.append(mtc)
+
+        # real train & test
+        clf.fit(self.X_train, self.y_train)  # 训练分类器
+        print(f'Train {name} Cost: {clf.get_training_time():.4f} s')
+
+        y_pred = clf.predict(self.X_test)
+
+        mtc = Metrics(self.y_test, y_pred, proba=False)  # 构建测试器
+        mtcs.append(mtc)
+        print(f'Testing {name} Cost: {clf.get_testing_time():.4f} s')
+
+        return mtcs, clf  # 返回所有测试器和分类器
+
+    def logline(self, name, mtcs: list, clf):
+        '''
+        将某次实验的结果写入日志df。
+        '''
+
+        test_mtc = mtcs.pop()
+
+        def getline(mtc):
+            func_list = []
+            for metric in self.metric_list:
+                func = getattr(mtc, metric, None)
+                if callable(func):
+                    func_list.append(func)
+                else:
+                    raise ValueError(f'{metric} is not in Metric.')
+
+            return [func() for func in func_list] + [clf.get_training_time(), clf.get_testing_time()]
+
+        self.test.loc[len(self.test)] = [name] + getline(test_mtc)  # 获取测试的结果
+
+        valid_rows = [getline(mtc) for mtc in mtcs]
+        valids_array = np.array(valid_rows)
+
+        mean_vals = np.mean(valids_array, axis=0).tolist()
+        std_vals = np.std(valids_array, axis=0).tolist()
+
+        valid_result = []
+        for mean, std in zip(mean_vals, std_vals):
+            valid_result.append(mean)
+            valid_result.append(std)
+
+        self.valid.loc[len(self.valid)] = [name] + valid_result
+
+    def save_df(self):
+        '''
+        保存df到日志
+        '''
+        self.test.to_csv(os.path.join(self.log_path, 'test.csv'), index=False)
+        self.valid.to_csv(os.path.join(self.log_path, 'valid.csv'), index=False)
+
+    def run_all(self, sort_by=['accuracy', 'accuracy_mean'], ascending=False):
+        '''
+        运行所有实验。
+
+        Parameters
+        ----------
+        sort_by : str
+            按照哪个指标进行排序。
+        ascending : bool
+            是否升序。
+        '''
+
+        for name, clf in self.clf_dict.items():
+            mtc, clf = self.excute(name, clf)
+
+            self.logline(name, mtc, clf)
+
+        if sort_by is not None:
+            print(self.test.sort_values(sort_by[0], ascending=ascending))
+            print(self.valid.sort_values(sort_by[1], ascending=ascending))
+        else:
+            print(self.test)
+            print(self.valid)
--- src/hw1/cifar-10/data_process.py
+++ src/hw1/cifar-10/data_process.py
@@ -4,21 +4,23 @@ import numpy as np
 # 加载CIFAR-10数据集 -> np.ndarray
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-np.random.seed(43)
+sed = np.random.randint(0, 1000)
+print(f'seed is {sed}')
+np.random.seed(sed)
 
 # 打乱训练集
 shuffle_indices = np.random.permutation(len(X_train))
-X_train = X_train[shuffle_indices]
+X_train = X_train[shuffle_indices] / 255.0
 y_train = y_train[shuffle_indices]
 
 # 打乱测试集
 shuffle_indices_test = np.random.permutation(len(X_test))
-X_test = X_test[shuffle_indices_test]
+X_test = X_test[shuffle_indices_test] / 255.0
 y_test = y_test[shuffle_indices_test]
 
 
-X_train = X_train.reshape(X_train.shape[0], -1)[:1000, :]
-y_train = y_train[:1000]
+X_train = X_train.reshape(X_train.shape[0], -1)[:, :]
+y_train = y_train[:]
 X_test = X_test.reshape(X_test.shape[0], -1)
 X_test = X_test.reshape(X_test.shape[0], -1)[:100, :]
 y_test = y_test[:100]
